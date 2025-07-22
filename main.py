8dceee6446c6080b71fa4da2b1eac0e035866a30
Bhavana Naidu <124422119+BhavanaNannuru@users.noreply.github.com>
2025-07-22 22:17:01 +0530

    Basic tkinter

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Code to generate colour palattes. 
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.title("Colour Palette Generator")
+root.geometry("300x600")
+
+
+select_filed = tk.StringVar()
+select = tk.OptionMenu(root, select_filed, "1", "2", "3", "4", "5")
+select.pack(side="top", pady=10)
+
+
+root.mainloop()
